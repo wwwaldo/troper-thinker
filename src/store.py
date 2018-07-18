c86110025786b08aac079635d152fbca2b017b53
Caroline Lin <ruoyi.lin@gmail.com>
2018-07-18 17:04:58 -0400

    bugfixes to Storage class
    * populate_from_text: change preprocessing line so newlines in file are
    ignored
    * expand: fix self.matrix failing to expand
    	result of np.resize was not reassigned to self.matrix.

--- src/store.py
+++ src/store.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pdb # the python debugger
 
 initial_tropes_size = 26000 
 # wc tropes.txt was 25855 last we checked
@@ -12,7 +12,9 @@ class Storage:
 
     @staticmethod
     def load(filename):
-        # load from an existing pickle file.
+        """
+        load from an existing pickle file. 
+        """
         with open(filename, "rb") as file:
             return pickle.load(file)
 
@@ -27,17 +29,17 @@ class Storage:
         self.num_media = 0
         self.trope_mapping = dict()
         self.media_mapping = dict()
-        self.matrix = None # fill this in using populate.
+        self.matrix = np.zeros((1,1), dtype=np.int8) # fill this in using populate.
         return
 
     def __str__(self):
-        return f"(#Tropes {self.num_tropes} #Media {self.num_media}) {self.trope_mapping} \n {self.media_mapping} \n {self.matrix}"
+        return f"(#Tropes {self.num_tropes} #Media {self.num_media}) \n {self.trope_mapping} \n {self.media_mapping} \n {self.matrix}"
 
     def populate_from_text(self, filename):
         # assumes: file contains only valid URLs and possibly newlines
         with open(filename, "r") as file: 
             urls = file.readlines()
-            urls = (url for url in urls if url) # preprocessing
+            urls = (url.strip() for url in urls if url.strip()) # preprocessing
 
             tropes_added = 0 
             for url in urls:
@@ -45,15 +47,15 @@ class Storage:
                 tropes_added += self.add_trope(token)
 
             if _debug:
-                print(f"Added {tropes_added} tropes to {self}") 
+                print(f"Added {tropes_added} tropes") 
             return
 
     def expand(self, dimension):
         # dimension == "tropes" or "media"
         if dimension == "tropes":
-            np.resize(self.matrix, (np.size(self.matrix, 0), 2 * np.size(self.matrix, 1)))
+            self.matrix = np.resize(self.matrix, (np.size(self.matrix, 0), 2 * np.size(self.matrix, 1)))
         elif dimension == "media":
-            np.resize(self.matrix, (2 * np.size(self.matrix, 0), np.size(self.matrix, 1)))
+            self.matrix = np.resize(self.matrix, (2 * np.size(self.matrix, 0), np.size(self.matrix, 1)))
         return
 
     def add_trope(self, trope):
@@ -61,6 +63,8 @@ class Storage:
         # returns: ntropes added to self.
 
         if trope in self.trope_mapping:
+            if _debug:
+                print(f"{trope} already in store")
             return 0
         else:
             self.num_tropes += 1
@@ -113,10 +117,66 @@ def query(self, trope):
     raise Exception("Function not implemented")
     return
 
+def main():
+    # create a new storage node.
+    store = Storage()
+    print(f"{store}")
+
+    # add a single trope to the store.
+    store.add_trope("Hello world")
+    # add a single trope to the store.
+    store.add_trope("Foo Bar Baz")
+    print(f"{store}")
+    ## bug --- Baz doesn't increase the column length. What?
+    ## fixed --- np.resize(matrix, (dim, dim)) doesn't mutate matrix, returns newmatrix.
+
+    # add a duplicate trope.
+    store.add_trope("Hello world")
+    print(f"{store}")
+
+    # add a real file of tropes.
+    store = Storage() #
+    store.populate_from_text("tropes.txt")
+    # print(f"{store}")
+    # looks good!
+
+    # try adding media.
+
+    return
+
 if __name__ == "__main__":
+    print("Hello world")
+    import doctest
+    doctest.testmod() # run doctests.
 
-    print("hello world")
+    main()
 
+"""
+footnote
+
+import store
+import pdb
+import importlib as imp
+
+# in a loop
+pdb.run(store.main())
+
+# on change to main:
+imp.reload(store)
+
+footnote(2):
+tried using ipdb by pip3 and ubuntu pkg install
+pip3 fails installation (no sudo)
+ubuntu pkg install works but is broken.
+    cannot run ipdb.set_trace
+    cannot run store.main() without importing store 
+    in ipdb code snippet
+    so ipdb does not inherit the interpreter context
+
+footnote(3):
+use :choose-tree to switch between panes and sessions in tmux.
+
+"""
 
 
 
