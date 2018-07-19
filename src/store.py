27ddf1ee0cca38871297410a2be6cea25ab99a41
Caroline Lin <ruoyi.lin@gmail.com>
2018-07-19 14:44:54 -0400

    Add get_score method to Storage class

--- src/store.py
+++ src/store.py
@@ -8,13 +8,24 @@ _debug = True
 class Storage:
     """
     Store trope data.
+
+    Attributes:
+        num_tropes, num_media (int) -- number of tropes, media currently in this store (excluding aliases)
+        trope, media (dict[string] : string) -- dict of tropes, media with corresponding unique name identifiers
+
+        _trope_mapping, _media_mapping : internal representation of trope : matrix index mapping
+        matrix : internal representation of tropes and media
+    
+    Methods:
+        TODO -- document methods.
+
     """
 
     @staticmethod
     def load(filename):
-        """
+        '''
         load from an existing pickle file. 
-        """
+        ''' 
         with open(filename, "rb") as file:
             return pickle.load(file)
 
@@ -25,8 +36,13 @@ class Storage:
         return
 
     def __init__(self):
+        '''
+        Initialize an empty store.
+        '''
         self.num_tropes = 0 
         self.num_media = 0
+        self.tropes = dict()
+        self.media = dict()
         self.trope_mapping = dict()
         self.media_mapping = dict()
         self.matrix = np.zeros((1,1), dtype=np.int8) # fill this in using populate.
@@ -34,7 +50,8 @@ class Storage:
 
     def __str__(self):
         return f"(#Tropes {self.num_tropes} #Media {self.num_media}) \n {self.trope_mapping} \n {self.media_mapping} \n {self.matrix}"
-
+    
+    # might 'deprecate' this later
     def populate_from_text(self, filename):
         # assumes: file contains only valid URLs and possibly newlines
         with open(filename, "r") as file: 
@@ -51,6 +68,9 @@ class Storage:
             return
 
     def expand(self, dimension):
+        '''
+        Method for dynamically resizing self.matrix. Internal use only. 
+        '''
         # dimension == "tropes" or "media"
         if dimension == "tropes":
             self.matrix = np.resize(self.matrix, (np.size(self.matrix, 0), 2 * np.size(self.matrix, 1)))
@@ -59,6 +79,12 @@ class Storage:
         return
 
     def add_trope(self, trope):
+        '''
+        Add a new trope to the store.
+
+        TODO -- handle trope aliasing.
+        '''
+
         # add a trope to self. trope is a string.
         # returns: ntropes added to self.
 
@@ -71,11 +97,16 @@ class Storage:
             if self.num_tropes > np.size(self.matrix, 1):
                 self.expand("tropes")
 
-            self.trope_mapping[trope] = self.num_tropes
+            self.trope_mapping[trope] = self.num_tropes - 1
 
             return 1
 
     def add_media(self, media):
+        '''
+        Add a new media to the store. 
+
+        TODO -- refactor this function to account for media aliases.
+        '''
         # add a media to self. 
 
         if media in self.media_mapping:
@@ -85,22 +116,60 @@ class Storage:
             if self.num_media > np.size(self.matrix, 1):
                 self.expand("media")
 
-            self.media_mapping[media] = self.num_media
+            self.media_mapping[media] = self.num_media - 1
 
             return 1
+    
+    def get_score(self, trope, media):
+        '''
+        Get trope score for this trope-media pair.
+        
+        Return: score as np.int8 if both trope and media exist in store, otherwise None.
+        '''
+        score = None
+        # if trope in self.tropes and media in self.media:
+        if trope in self.trope_mapping and media in self.media_mapping:
+            trope_idx = self.trope_mapping[trope] # TODO -- refactor for aliasing
+            media_idx = self.media_mapping[media]
 
-    def update(self, trope, medialist):
-        # input: dict of Media strings, with scores, and a trope name.
-        # logs errors. permissively assumes media strings correspond to valid media names, which are unique identifiers. creates new entries in internal representation if media is not found.
+            score = self.matrix[trope_idx][media_idx]
+        return score
+
+    def update(self, trope, media_scores):
+        '''
+        Update a trope with media scores.
+
+        Params:
+        trope (string) -- Unique name identifier of this trope.
+        media_scores dict[string] -- dictionary of media names : trope-media scores.
+            a trope-media score is positive if trope is played straight in media
+            and zero if trope is not invoked in media.
+        
+        Example:
+
+        >>> store = Storage()
+        >>> trope = "JumpingTheShark"
+        >>> store.add_trope(trope)
+        1
+        >>> show = "TheSimpsons"
+        >>> store.add_media(show)
+        1
+        >>> media_scores = {show : 1}
+        >>> store.update(trope, media_scores)
+        >>> print(store.get_score(trope, show))
+        1
+        
+        Notes: Permissively assumes media strings correspond to valid media names, which are unique identifiers. 
+        Creates new entries in internal representation if media is not found.
+        '''
         if trope in self.trope_mapping:
             trope_index = self.trope_mapping[trope]
 
-            for media in medialist.keys():
+            for media in media_scores.keys():
                 if not media in self.media_mapping:
                     self.add_media(media)
-
                 media_index = self.media_mapping[media]
-                recorded_score = medialist[media]
+                recorded_score = media_scores[media]
                 self.matrix[media_index][trope_index] = recorded_score
         else:
             if _debug:
@@ -113,9 +182,9 @@ class Storage:
         raise Exception("Function not implemented")
         return
 
-def query(self, trope):
-    raise Exception("Function not implemented")
-    return
+    def query(self, trope):
+        raise Exception("Function not implemented")
+        return
 
 def main():
     # create a new storage node.
@@ -150,6 +219,15 @@ if __name__ == "__main__":
     doctest.testmod() # run doctests.
 
     main()
+
+    store = Storage()
+    trope = "JumpingTheShark"
+    store.add_trope(trope)
+    show = "TheSimpsons"
+    store.add_media(show)
+    media_scores = {show : 1}
+    store.update(trope, media_scores)
+    print(store.get_score(trope, show))
 
 """
 footnote
